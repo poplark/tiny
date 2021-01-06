0d464e7b79bd990db10ed37e3796c6773c848730
poplark <songkang666@126.com>
2021-01-06 20:05:27 +0800

    chore: lerna

---
+++ rollup.base.js
@@ -0,0 +1,45 @@
+import json from '@rollup/plugin-json';
+// import typescript from '@rollup/plugin-typescript';
+import typescript from 'rollup-plugin-typescript2';
+import resolve from '@rollup/plugin-node-resolve';
+// import replace from '@rollup/plugin-replace';
+import commonjs from '@rollup/plugin-commonjs';
+// import * as path from 'path';
+// import { name, main, module, version } from './package.json';
+import tsconfig from './tsconfig.json';
+
+export default {
+  input: 'src/index.ts',
+  // output: [
+  //   {
+  //     format: 'umd',
+  //     // dir: path.parse(main).dir,
+  //     file: main,
+  //     name,
+  //   },
+  //   {
+  //     format: 'esm',
+  //     // dir: path.parse(module).dir,
+  //     file: module,
+  //     // }, {
+  //     //   format: 'cjs',
+  //     //   dir: 'cjs',
+  //     // }, {
+  //     //   dir: 'iffe',
+  //     //   format: 'iife',
+  //     //   name: 'myRollup',
+  //   },
+  // ],
+  plugins: [
+    json(),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfigDefaults: tsconfig,
+      useTsconfigDeclarationDir: true,
+    }),
+    // replace({
+    //   __VERSION__: JSON.stringify(version),
+    // }),
+  ],
+};
