c1c8ff02b354c3bfb3b99c6bbfc8f9655ad4d780
poplark <songkang666@126.com>
2021-01-07 01:57:14 +0800

    fix: add context for throttled function

--- packages/lodash/src/throttle.ts
+++ packages/lodash/src/throttle.ts
@@ -6,11 +6,13 @@ export function throttle<T extends (...args: any[]) => any>(fn: T, wait?: number
   let timer;
   const _wait = wait || 1000;
   const throttled: ThrottledFunc<T> = function (...args: any[]): any {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const ctx = this;
     if (timer) {
       return;
     }
     timer = setTimeout(() => {
-      fn(...args);
+      fn.call(ctx, ...args);
       timer = null;
     }, _wait);
   };
